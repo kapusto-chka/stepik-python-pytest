979fc779b963cd650a27a0198a94a16aeea3ee6e
kapusto-chka <kapustinmarat@gmail.com>
2021-06-17 13:50:02 +0300

    добавил from selenium.webdriver.common.by import By

--- education/section_1/test_lesson_1_6_4.py
+++ education/section_1/test_lesson_1_6_4.py
@@ -1,5 +1,7 @@
-from selenium import webdriver
 import time
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
 
 link = "http://suninjuly.github.io/simple_form_find_task.html"
 
@@ -7,15 +9,15 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element_by_tag_name('.form-control[name="first_name"]')
+    input1 = browser.find_element(By.CSS_SELECTOR, '.form-control[name="first_name"]')
     input1.send_keys("Ivan")
-    input2 = browser.find_element_by_name('.form-control[name="last_name"]')
+    input2 = browser.find_element(By.CSS_SELECTOR, '.form-control[name="last_name"]')
     input2.send_keys("Petrov")
-    input3 = browser.find_element_by_class_name('.form-control.city')
+    input3 = browser.find_element(By.CSS_SELECTOR, '.form-control.city')
     input3.send_keys("Smolensk")
-    input4 = browser.find_element_by_id('#country')
+    input4 = browser.find_element(By.CSS_SELECTOR, '#country')
     input4.send_keys("Russia")
-    button = browser.find_element_by_css_selector("button.btn")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
 finally:
