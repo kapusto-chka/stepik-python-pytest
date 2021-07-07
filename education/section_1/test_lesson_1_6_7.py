afd880637b0f76336d27141462c89898217a6a48
kapusto-chka <kapustinmarat@gmail.com>
2021-07-07 12:15:00 +0300

    дополнил алгоритм

--- education/section_1/test_lesson_1_6_7.py
+++ education/section_1/test_lesson_1_6_7.py
@@ -5,7 +5,7 @@ import time
 try:
     browser = webdriver.Chrome()
     browser.get("http://suninjuly.github.io/huge_form.html")
-    elements = browser.find_elements_by(By.CSS_SELECTOR, '[type="text"]')
+    elements = browser.find_elements(By.CSS_SELECTOR, '[type="text"]')
     for element in elements:
         element.send_keys("Мой ответ")
 
@@ -14,7 +14,7 @@ try:
 
 finally:
     # успеваем скопировать код за 30 секунд
-    time.sleep(30)
+    time.sleep(5)
     # закрываем браузер после всех манипуляций
     browser.quit()
 
