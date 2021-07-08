1319e3142fe01c172e54e6e0cff3deba3b57a6dc
kapusto-chka <kapustinmarat@gmail.com>
2021-07-08 17:13:43 +0300

    добавил файл с решением к 1_6_11

---
+++ education/section_1/test_lesson_1_6_11.py
@@ -0,0 +1,41 @@
+# внутри написал условие, по которому будет определяться ссылка (на норм страницу и страницу с багом)
+# нужно ввеести цифру 1 или 2 на первом этапе 1 - откроется страница без бага; 2 - откроется страница с багом
+
+
+from selenium import webdriver
+import time
+
+link_1 = 'http://suninjuly.github.io/registration1.html'
+link_2 = 'http://suninjuly.github.io/registration2.html'
+
+x = int(input('Введите число 1 или 2: '))
+
+if x == 1:
+    browser = webdriver.Chrome().get(link_1)
+elif x == 2:
+    browser = webdriver.Chrome().get(link_2)
+
+else:
+    print('вы не справились')
+    exit()
+
+try:
+    # browser = webdriver.Chrome()
+    input1 = browser.find_element_by_css_selector('.first_block .form-control.first')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_css_selector('.first_block .form-control.second')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_css_selector('.first_block .form-control.third')
+    input3.send_keys("ivanpetrov@testmail.com")
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+    time.sleep(2)
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    welcome_text = welcome_text_elt.text
+
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    time.sleep(10)
+    browser.quit()
