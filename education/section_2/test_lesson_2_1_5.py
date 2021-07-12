a1b75f672aed15fa8d3f51105bd1bc89103a4bac
kapusto-chka <kapustinmarat@gmail.com>
2021-07-12 13:23:45 +0300

    добавил файл с решением к 2_1_5

--- education/section_2/test_lesson_2_1_5.py
+++ education/section_2/test_lesson_2_1_5.py
@@ -22,17 +22,7 @@ try:
     option1 = browser.find_element(By.CSS_SELECTOR, '#robotCheckbox').click()
     option2 = browser.find_element(By.CSS_SELECTOR, '#robotsRule').click()
 
-
-
-
-
-    # button = browser.find_element_by_css_selector("button.btn")
-    # button.click()
-    # time.sleep(2)
-    # welcome_text_elt = browser.find_element_by_tag_name("h1")
-    # welcome_text = welcome_text_elt.text
-    #
-    # assert "Congratulations! You have successfully registered!" == welcome_text
+    button = browser.find_element_by_css_selector(".btn.btn-default").click()
 
 finally:
     time.sleep(10)
