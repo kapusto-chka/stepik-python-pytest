8e335ec2f899eb6c20afff8271e6c6e10a91c4dc
kapusto-chka <kapustinmarat@gmail.com>
2021-07-12 13:09:22 +0300

    добавил файл с решением к 2_1_5

--- education/section_1/test_lesson_1_6_11.py
+++ education/section_1/test_lesson_1_6_11.py
@@ -1,35 +1,66 @@
 # внутри написал условие, по которому будет определяться ссылка (на норм страницу и страницу с багом)
 # нужно ввеести цифру 1 или 2 на первом этапе 1 - откроется страница без бага; 2 - откроется страница с багом
 
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
 
 link_1 = 'http://suninjuly.github.io/registration1.html'
 link_2 = 'http://suninjuly.github.io/registration2.html'
+# x = int(input('Введите число 1 или 2: '))
 
-x = int(input('Введите число 1 или 2: '))
+# if x == 1:
+#     browser = webdriver.Chrome()
+#     browser.get(link_1)
+# elif x == 2:
+#     browser = webdriver.Chrome()
+#     browser.get(link_2)
+# else:
+#     print('вы не справились')
+#     exit()
 
-if x == 1:
-    browser = webdriver.Chrome().get(link_1)
-elif x == 2:
-    browser = webdriver.Chrome().get(link_2)
-else:
-    print('вы не справились')
-    exit()
 try:
-    input1 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your first name"]').send_keys('Ivan')
-    input2 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your last name"]').send_keys('Ivanov')
-    input3 = browser.find_element(By.CSS_SELECTOR, '[placeholder="Input your email"]').send_keys('i.ivanov@testmail.com')
-    button = browser.find_element(By.CSS_SELECTOR, 'button.btn').click()
+    browser = webdriver.Chrome()
+    browser.get(link_1)
+    input1 = browser.find_element_by_css_selector('.first_block .form-control.first')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_css_selector('.first_block .form-control.second')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_css_selector('.first_block .form-control.third')
+    input3.send_keys("ivanpetrov@testmail.com")
 
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
     time.sleep(2)
-
-    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
     welcome_text = welcome_text_elt.text
 
     assert "Congratulations! You have successfully registered!" == welcome_text
 
 finally:
-    time.sleep(5)
+    time.sleep(10)
+    browser.quit()
+
+
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link_2)
+    input1 = browser.find_element_by_css_selector('.first_block .form-control.first')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_css_selector('.first_block .form-control.second')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_css_selector('.first_block .form-control.third')
+    input3.send_keys("ivanpetrov@testmail.com")
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+    time.sleep(2)
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    welcome_text = welcome_text_elt.text
+
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    time.sleep(10)
     browser.quit()
